3e1a3dcfa767ea54804f4328fa63ce68d0d12087
Vittorio Ballestra <vittorio.ballestra@drafintech.it>
2016-10-02 23:09:53 +0200

    register dart extension in order to create it

--- lib/my_tag.dart
+++ lib/my_tag.dart
@@ -2,6 +2,21 @@ import 'dart:js';
 import 'dart:html';
 import 'package:polymer_element/polymer_element.dart' as polymer;
 
+class MyData {
+  String _f;
+  String field1;
+  String get field2 => _f;
+  set field2(String v) {
+    print("SETTING F2 : ${v}");
+    _f = v;
+  }
+
+  MyData({String field1,String field2}) {
+    this.field1 = field1;
+    this.field2 = field2;
+  }
+}
+
 abstract class MyDartTag extends polymer.Element {
   static const TAG='my-tag';
 
@@ -9,11 +24,25 @@ abstract class MyDartTag extends polymer.Element {
 
   int count=0;
 
+  MyData data1;
+
   String something = "hi";
+
+  List elems = [
+    new MyData(field1:'a1',field2:'b1'),
+    new MyData(field1:'a2',field2:'b2'),
+    new MyData(field1:'a3',field2:'b3'),
+  ];
+
 
   MyDartTag()  {
     print("Hi");
+    data1 = new MyData()
+      ..field1 = "uno"
+      ..field2 = "due";
   }
+
+  factory MyDartTag.create() => new Element.tag(TAG);
 
   hello(Event ev,details) {
     ev.preventDefault();
@@ -21,9 +50,15 @@ abstract class MyDartTag extends polymer.Element {
     print("My Parent : ${this.parentNode}");
     print("Bounding box ${this.getBoundingClientRect()}");
 
+
+
     // Magical getters and setters in action here !
     count = count + 1;
     print("Count is : ${count}");
+  }
+
+  addAnother(Event ev,details) {
+    this.parentNode.append(new MyDartTag.create());
   }
 
 }
