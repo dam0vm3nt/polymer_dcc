fe5d4f18cbe3ac6f82799bda44a00ec081ff98be
Vittorio Ballestra <vittorio.ballestra@drafintech.it>
2016-10-05 11:52:22 +0200

    changes

--- lib/my_tag.dart
+++ lib/my_tag.dart
@@ -12,39 +12,36 @@ class MyData {
     _f = v;
   }
 
-  MyData({String field1,String field2}) {
+  MyData({String field1, String field2}) {
     this.field1 = field1;
     this.field2 = field2;
   }
 }
 
-class MyDartTag extends PolymerElement {
-  static const TAG='my-tag';
 
-  static JsObject CONFIG = new JsObject.jsify({
-    'observers' : [
-      'countChanged(count)'
-    ]
-  });
+class MyDartTag extends PolymerElement {
+  static const TAG = 'my-tag';
+  static Config CONFIG = new Config(observers: ['countChanged(count)']);
 
   int _count;
-  int get count=> _count;
+  int get count => _count;
   set count(int v) {
     print("SETTER !!! Setting count ${v}");
     _count = v;
+    _logMe("Setted : ${v}");
   }
 
-  _logMe(v)  {
-    new Future.delayed(new Duration(milliseconds:5000)).then((_) {
-      print("Delayed setted log :${v}");
-    });
+  _logMe(v) async {
+    await new Future.delayed(new Duration(milliseconds: 500));
+    print("Delayed setted log :${v}");
   }
 
   MyData data1;
 
-  void attached() {
+  void connectedCallback() {
+    super.connectedCallback();
     print("ATTACHED!!");
-
+    count=0;
   }
 
   void countChanged(int newVal) {
@@ -54,52 +51,40 @@ class MyDartTag extends PolymerElement {
   String something = "hi";
 
   List cippi = [
-    new MyData(field1:'a1',field2:'b1'),
-    new MyData(field1:'a2',field2:'b2'),
-    new MyData(field1:'a3',field2:'b3'),
+    new MyData(field1: 'a1', field2: 'b1'),
+    new MyData(field1: 'a2', field2: 'b2'),
+    new MyData(field1: 'a3', field2: 'b3'),
   ];
 
-
-  MyDartTag()  {
+  MyDartTag() {
     print("Hi");
-    data1 = new MyData(field1: 'uno',field2: 'due');
-    count = 1;
+    data1 = new MyData(field1: 'uno', field2: 'due');
+    // Setter works too
+    set('count',1);
+    //count = 1;
   }
 
   factory MyDartTag.create() => new Element.tag(TAG);
 
-  hello(Event ev,details) {
+  hello(Event ev, details) {
     ev.preventDefault();
     print("hello :${ev} , ${details}");
     print("My Parent : ${this.parentNode}");
     print("Bounding box ${this.getBoundingClientRect()}");
 
     // Magical getters and setters in action here !
-    if (count == null) {
-      count = 0;
-    }
     count = count + 1;
     if (cippi == null) {
       cippi = [];
     }
-    cippi.add(new MyData(field1:'x',field2:'y'));
+    cippi.add(new MyData(field1: 'x', field2: 'y'));
     // Equivalent to notify (less performant)
     cippi = cippi;
     print("Count is : ${count}");
-    new MyLogger().logMe("Bye!!!");
-    //_logMe("ciao");
+    _logMe("ciao");
   }
 
-  addAnother(Event ev,details) {
+  addAnother(Event ev, details) {
     this.parentNode.append(new MyDartTag.create());
-  }
-
-}
-
-class MyLogger {
-  logMe(v)  {
-    new Future.delayed(new Duration(milliseconds:5000)).then((_) {
-      print("Delayed setted log :${v}");
-    });
   }
 }
